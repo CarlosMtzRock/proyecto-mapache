2b70483161aeb69544cfd1fd19b70494ed6c3e86
carlosMtzRock <carlosheavymetal66@gmail.com>
2025-12-01 18:48:14 -0600

    primer commit

---
+++ src/main/java/mx/uacm/edu/proyecto/proyectofinal/ProyectoFinalMapeoApplication.java
@@ -0,0 +1,13 @@
+package mx.uacm.edu.proyecto.proyectofinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalMapeoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoFinalMapeoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mx/uacm/edu/proyecto/proyectofinal/ProyectoFinalMapeoApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.uacm.edu.proyecto.proyectofinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalMapeoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
